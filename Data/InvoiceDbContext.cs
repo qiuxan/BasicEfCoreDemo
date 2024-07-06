b189acae085cd2307e609ee62a248a3ffb813795
qiuxan <qiuxan@gmail.com>
2024-07-06 11:33:45 +1000

    update of the immigrate to create a invoice table in localdb

---
+++ Data/InvoiceDbContext.cs
@@ -0,0 +1,10 @@
+﻿using BasicEfCoreDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicEfCoreDemo.Data
+{
+    public class InvoiceDbContext(DbContextOptions<InvoiceDbContext> options):DbContext(options)
+    {
+        public DbSet<Invoice> Invoices { get; set; }
+    }
+}
---
+++ Models/Invoice.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BasicEfCoreDemo.Models
+{
+    public class Invoice
+    {
+        [Column("Id")]
+        [Key]
+        public Guid Id { get; set; }
+        public string InvoiceNumber { get; set; } = string.Empty;
+        public string ContactName { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public decimal Amount { get; set; }
+        public DateTimeOffset InvoiceDate { get; set; }
+        public DateTimeOffset DueDate { get; set; }
+        public InvoiceStatus Status { get; set; }
+    }
+}
+
+public enum InvoiceStatus
+{
+    Draft,
+    Sent,
+    Paid,
+    Overdue
+}
